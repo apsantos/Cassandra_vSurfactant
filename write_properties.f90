8e43bfaa19f7d01cacb3e746f8cfa380230d35f4
Andrew Santos <apsantos@princeton.edu>
2017-05-12 17:26:28 -0400

    adjust histogram writing format again

--- write_properties.f90
+++ write_properties.f90
@@ -552,8 +552,8 @@ SUBROUTINE Write_Histogram(this_box)
       histogram_file = 'his_'//TRIM(run_name)// '.spec' // TRIM(Int_To_String(is)) //'.dat'
       OPEN(unit=this_unit, file=histogram_file)
 
-      WRITE (this_unit,'(A)') '    T       mu          width     x- y- z-dim  '
-      WRITE (this_unit,'(3f7.4,3f8.3)') 1./beta(this_box), species_list(is)%chem_potential, energy_hist_width, &
+      WRITE (this_unit,'(A)') '    kBT       mu          width     x- y- z-dim  (Atomistic units)'
+      WRITE (this_unit,'(3f11.3,3f8.3)') 1./beta(this_box), species_list(is)%chem_potential, energy_hist_width, &
                                         box_list(this_box)%length(1,1), &
                                         box_list(this_box)%length(2,2), &
                                         box_list(this_box)%length(3,3)
